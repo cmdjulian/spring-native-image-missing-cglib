becb23fed12997e4c6c2ad184b923a19bf099c62
cmdjulian <julian.goede@pm.me>
2024-04-15 22:57:44 +0200

    compacted reproducer

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation(project(":autoconfig"))
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
